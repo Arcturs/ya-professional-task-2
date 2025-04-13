96c9774f4029d89eea7cf25c51ffc4d248872c9b
a.sashina <yaninastya2010@yandex.ru>
2025-04-13 12:06:23 +0300

    добавление забытых файлов

--- src/main/java/ru/spb/itmo/asashina/yaprofessionaltask2/YaProfessionalTask2Application.java
+++ src/main/java/ru/spb/itmo/asashina/yaprofessionaltask2/YaProfessionalTask2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YaProfessionalTask2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YaProfessionalTask2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YaProfessionalTask2Application.class, args);
+    }
 
 }
--- src/test/java/ru/spb/itmo/asashina/yaprofessionaltask2/YaProfessionalTask2ApplicationTests.java
+++ src/test/java/ru/spb/itmo/asashina/yaprofessionaltask2/YaProfessionalTask2ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class YaProfessionalTask2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
